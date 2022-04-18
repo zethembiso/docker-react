e3df209f9ff7508e03ea720a20f354df0d371719
zethembiso <97094554+zethembiso@users.noreply.github.com>
2022-04-18 21:30:27 +0200

    fixed tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,13 +1,13 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders learn react text', () => {
   render(<App />);
   const linkElement = screen.getByText(/I was changed/i);
   expect(linkElement).toBeInTheDocument();
 });
 
-test('renders learn react link', () => {
+test('renders learn react text', () => {
   render(<App />);
   const linkElement = screen.getByText(/I was changed/i);
   expect(linkElement).toBeInTheDocument();
